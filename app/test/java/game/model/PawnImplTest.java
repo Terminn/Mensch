7a6cdca56bb9a54904d9c874659e7ddccdf85088
Till Rouvier <till.rouvier@gmail.com>
2019-05-22 14:54:45 +0200

    added tests for pawnimpl and playerimpl

---
+++ app/test/java/game/model/PawnImplTest.java
@@ -0,0 +1,49 @@
+package game.model;
+
+import org.junit.Test;
+
+import game.gameExceptions.PawnCannotMoveException;
+
+import static org.junit.Assert.*;
+
+public class PawnImplTest {
+
+    Pawn pawn = new PawnImpl(PawnColor.Red);
+
+    @Test
+    public void newPawnImpl(){
+        assertNotNull(new PawnImpl(PawnColor.Red));
+    }
+
+    @Test
+    public void getIndexAtIndex() {
+        for (int i = 0; i < 4; i++) {
+            assertEquals(0, pawn.getIndexAtIndex(i));
+        }
+    }
+
+    @Test
+    public void getColor() {
+        assertEquals(PawnColor.Red,pawn.getColor());
+    }
+
+    @Test
+    public void getsEaten() {
+        pawn.getsEaten(0);
+        assertEquals(0,pawn.getIndexAtIndex(0));
+    }
+
+    @Test
+    public void movePawn() {
+        pawn.movePawn(6,0);
+        assertEquals(6,pawn.getIndexAtIndex(0));
+    }
+
+    @Test
+    public void canMoveSpecificPawn() {
+    }
+
+    @Test
+    public void canMove() {
+    }
+}
